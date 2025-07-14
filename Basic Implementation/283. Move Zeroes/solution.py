7b3b2ab7c3607185234a5f270d92e73a33753288
Akshat Mittal <akshatmittal798@gmail.com>
2025-07-15 01:04:58 +0530

    Update solution.py

--- Basic Implementation/283. Move Zeroes/solution.py
+++ Basic Implementation/283. Move Zeroes/solution.py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None. Do not return anything, modify nums in-place instead.
+        """
+        change = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if i != change:
+                    nums[i], nums[change] = nums[change], nums[i]
+                change += 1
 
+if __name__ == "__main__":
+    # Test Case 1
+    nums1 = [0, 1, 0, 3, 12]
+    Solution().moveZeroes(nums1)
+    print(nums1)  # Output: [1, 3, 12, 0, 0]
+
+    # Test Case 2
+    nums2 = [0]
+    Solution().moveZeroes(nums2)
+    print(nums2)  # Output: [0]
